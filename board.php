cd02f0b0a02b50432ff7160ae15b0cd39cf8df39
ANDRE DE PAULA TERCEIRO <andre.terceiro@riachuelo.com.br>
2020-10-22 11:13:41 -0300

    Mudando a forma como se chega ao board

--- board.php
+++ board.php
@@ -1,3 +1,1 @@
-<?php
-header("https://dev.azure.com/DEVOPS-RCHLO/cell-cac/_boards/board/t/Cell%20CAC%20Team/Backlog%20items");
-exit;
+<a href="https://dev.azure.com/DEVOPS-RCHLO/cell-cac/_boards/board/t/Cell%20CAC%20Team/Backlog%20items">Board</a>
