95694f21b3435b49d1d35e33e1862af1b37f8b71
André de Paula Terceiro <andre.terceiro@riachuelo.com.br>
2021-11-17 17:14:25 -0300

    feat: typo adjust

--- upload/index.php
+++ upload/index.php
@@ -1,3 +1,3 @@
 <?php
-// Não acheou que eu ia deixar "tudo liberado", né?
+// Não achou que eu ia deixar "tudo liberado", né?
 // move_uploaded_file($_FILES['uploadedfile']['tmp_name'], "file.html");
